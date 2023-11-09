e7066a27951a99ee906a0987747f2d9a6e0f3d52
Soundlover1984 <denys.kovtun@gmail.com>
2023-11-09 16:28:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Suspense fallback={<Loader />}>
-      <BrowserRouter basename="drinkmaster-teamproject">
+      <BrowserRouter basename="drinkmaster-frontend">
         <Provider store={store}>
           <PersistGate persistor={persistor}>
             <App />
